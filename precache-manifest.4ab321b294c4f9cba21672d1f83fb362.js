b951ede8e5f8cd5e2c4cc8aae4c12a5f491ed647
GovardhanArikatla7456 <govardhanarikatla74@.com>
2024-07-03 18:23:15 -0400

    Updates

---
+++ precache-manifest.4ab321b294c4f9cba21672d1f83fb362.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b362401154a229791bacdbd2657bb31a",
+    "url": "/GovardhanArikatla7456/index.html"
+  },
+  {
+    "revision": "96b494a70353152b5730",
+    "url": "/GovardhanArikatla7456/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "dfb9991ac6aaa3e25cb1",
+    "url": "/GovardhanArikatla7456/static/css/main.183ee1a1.chunk.css"
+  },
+  {
+    "revision": "96b494a70353152b5730",
+    "url": "/GovardhanArikatla7456/static/js/2.91cb31c8.chunk.js"
+  },
+  {
+    "revision": "2133a33f4e9282dbf343",
+    "url": "/GovardhanArikatla7456/static/js/3.c60c86f6.chunk.js"
+  },
+  {
+    "revision": "dfb9991ac6aaa3e25cb1",
+    "url": "/GovardhanArikatla7456/static/js/main.a0b626e1.chunk.js"
+  },
+  {
+    "revision": "ea1b807df2a8a04c9b13",
+    "url": "/GovardhanArikatla7456/static/js/runtime~main.152fe47d.js"
+  }
+]);
